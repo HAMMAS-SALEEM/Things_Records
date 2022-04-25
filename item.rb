01421c633c0ef485a5f8b21d8d0b6fad4585705e
farouqdaif1 <daiffarouq@gmail.com>
2022-04-25 21:59:44 +0200

    Add item class

---
+++ item.rb
@@ -0,0 +1,35 @@
+class Item
+  attr_accessor :publish_date
+
+  def initialize(_id, publish_date, archived: false)
+    @id = Random.rand(1..1000)
+    @publish_date = publish_date
+    @archived = archived
+  end
+
+  def genre(genre)
+    @genre = genre
+  end
+
+  def author(author)
+    @author = author
+  end
+
+  def source(source)
+    @source = source
+  end
+
+  def label(label)
+    @label = label
+  end
+
+  def move_to_archivd
+    @archived = true if can_be_archived?
+  end
+
+  private
+
+  def can_be_archived?
+    return true if Time.new.year - published_date > 10
+  end
+end
