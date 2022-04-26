1c49e6af6f021a5989402811a3d20e05b6b3c52c
Mphatso Lichapa <lichapau@gmail.com>
2022-04-26 10:31:19 +0200

    Added user options on main.rb

--- main.rb
+++ main.rb
@@ -1,7 +1,18 @@
 class Main
   def start
-    puts 'Welcome the Things Records App
-    0 - List Books'
+    puts " Welcome the Things Records App
+    1  - List all books
+    2  - List all music albums
+    3  - List of games
+    4  - List all genres (e.g 'Comedy', 'Thriller')
+    5  - List all labels (e.g. 'Gift', 'New')
+    6  - List all authors (e.g. 'Stephen King')
+    7  - List all sources (e.g. 'From a friend', 'Online shop')
+    8  - Add a book
+    9  - Add a music album
+    10 - Add a movie
+    11 - Quit the App
+    "
   end
 end
 
