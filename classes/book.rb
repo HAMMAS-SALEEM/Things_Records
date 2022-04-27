5cf5cef17b5e49b3741d5481ec3ac8dc1af9bf1b
HAMMAS-SALEEM <hammassaleem376@gmail.com>
2022-04-27 22:45:21 +0500

    :art: Updated Book Class

--- classes/book.rb
+++ classes/book.rb
@@ -1,16 +1,15 @@
 require_relative 'item'
 
 class Book < Item
-  def initialize(publisher, cover_state)
+  def initialize(publisher, cover_state, publish_date)
     @publisher = publisher
     @cover_state = cover_state
-    super()
+    super(publish_date)
   end
 
   private
 
   def can_be_archived?
-    return true if publish_date - Time.new.year > 10
-    return true if @cover_state == 'bad'
+    true if @publish_date - Time.new.year > 10 || @cover_state == 'bad'
   end
 end
