2e1701a6d0759c7b014261fa2a480077aed76da5
HAMMAS-SALEEM <hammassaleem376@gmail.com>
2022-04-28 15:12:33 +0500

    :tada: Added Book and Label Section Completely

--- app_functionality/app_game.rb
+++ app_functionality/app_game.rb
@@ -1,11 +1,12 @@
-require_relative '../store_data/game_store'
+require './classes/game'
 require_relative 'app_input'
+require './store_data/preserve_data'
 module AppGame
-  include GameStore
   include Input
+  include PreserveData
 
   def list_all_games
-    games = load_game
+    games = load_data('games')
     puts('No Games Avalibale') if games.empty?
     games.each_with_index do |game, index|
       puts(
@@ -16,7 +17,9 @@ module AppGame
 
   def add_game
     input = inp(['multiplayer', "last played at (e.g.'2022-02-27')", "publish  date (e.g.'2022-02-27')"])
-    write_game(input[0], input[1], input[2])
+    game = Game.new(input[0], input[1], input[2])
+    game_obj = { multiplayer: game.multiplayer, last_played_at: game.last_played_at, publish_date: game.publish_date }
+    update_data('games', game_obj)
     puts('Game created Successfully')
   end
 end
--- classes/app.rb
+++
@@ -1,3 +0,0 @@
-require './main'
-
-puts 'Under Development'
